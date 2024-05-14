a2feae3e4f2e65c22d0dd0ef3dd4c26746308689
Guillermo Simon <guillermosimon.2018@gmail.com>
2024-05-14 16:04:38 -0300

    Commit antes de mandarme cualquiera

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!"); 
+int a; 
+int b; 
+a=10; 
+b=a; 
+Console.WriteLine("valor de a:"+a);
+Console.WriteLine("valor de b:"+b);
+
+//Ejercicio 1
+
+string cadena;
+cadena = Console.ReadLine();
+int numero;
+bool resultado = int.TryParse(cadena, out numero);
+if (resultado)
+{   
+    int invertido=0;
+    int digito;
+    if (numero > 0)
+    {
+        while (numero > 0)
+        {
+            digito = numero%10;
+            invertido = invertido *10 + digito;
+            numero /=10;
+        }
+    }
+Console.WriteLine("Valor del invertido: "+invertido);    
+}
+
+
+
+
